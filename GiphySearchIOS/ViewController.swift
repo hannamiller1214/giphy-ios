17842b17938139110ca8fb9a3826bec4d96ce551
Hanna Miller <hanna.miller@students.dominican.edu>
2020-04-06 19:28:28 -0700

    Initial Commit

---
+++ GiphySearchIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearchIOS
+//
+//  Created by Hanna Miller on 4/6/20.
+//  Copyright © 2020 Hanna Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
